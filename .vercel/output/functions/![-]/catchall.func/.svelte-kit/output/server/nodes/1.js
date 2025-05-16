be64972186e513a4109f92843aa4f69cc1f0251e
Alessandro Bessi <abessi@nextbit.it>
2025-05-16 11:25:20 +0200

    remove .vercel folder

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CLi302Li.js","_app/immutable/chunks/abR5r1hs.js","_app/immutable/chunks/AfbuXoTy.js","_app/immutable/chunks/C9ygN-CC.js","_app/immutable/chunks/BhgL6my-.js","_app/immutable/chunks/_9IsAGxx.js","_app/immutable/chunks/Czefwyi5.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.Dlld9wRc.js","_app/immutable/chunks/abR5r1hs.js","_app/immutable/chunks/AfbuXoTy.js","_app/immutable/chunks/C9ygN-CC.js","_app/immutable/chunks/BhgL6my-.js","_app/immutable/chunks/Dx1usP5A.js","_app/immutable/chunks/Czefwyi5.js"];
-export const stylesheets = ["_app/immutable/assets/2.O2Od2821.css"];
-export const fonts = [];
