1bd2dab1b8659a89850bb9fdb4b0b68fe90bcc58
Alessandro Bessi <abessi@nextbit.it>
2025-05-19 11:12:54 +0200

    import wasm 0.2.1

--- .svelte-kit/output/client/_app/immutable/entry/start.SklTFsgN.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/ESdLhv7Q.js";export{o as load_css,r as start};
--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.ico"]),
 	mimeTypes: {},
 	_: {
-		client: {start:"_app/immutable/entry/start.SklTFsgN.js",app:"_app/immutable/entry/app.Dp1X1nxm.js",imports:["_app/immutable/entry/start.SklTFsgN.js","_app/immutable/chunks/ESdLhv7Q.js","_app/immutable/chunks/75EMG3Uh.js","_app/immutable/chunks/BCr4zGHm.js","_app/immutable/entry/app.Dp1X1nxm.js","_app/immutable/chunks/Bqm4yP9Y.js","_app/immutable/chunks/75EMG3Uh.js","_app/immutable/chunks/CrSiB_9q.js","_app/immutable/chunks/BE8XQIoG.js","_app/immutable/chunks/BCr4zGHm.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.N4Ula_RV.js",app:"_app/immutable/entry/app.iy-cTytJ.js",imports:["_app/immutable/entry/start.N4Ula_RV.js","_app/immutable/chunks/SRgk6SyT.js","_app/immutable/chunks/75EMG3Uh.js","_app/immutable/chunks/BCr4zGHm.js","_app/immutable/entry/app.iy-cTytJ.js","_app/immutable/chunks/Bqm4yP9Y.js","_app/immutable/chunks/75EMG3Uh.js","_app/immutable/chunks/CrSiB_9q.js","_app/immutable/chunks/BE8XQIoG.js","_app/immutable/chunks/BCr4zGHm.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
